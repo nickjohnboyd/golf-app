deb73d18d68e6af20e9a10d5cac73db4e4a713c4
Nick Boyd <nickboyd@Nicks-MacBook-Pro.local>
2019-11-18 22:55:39 -0700

    first courses display added

--- js/action.js
+++ js/action.js
@@ -1,14 +1,19 @@
-let allCourses;
-let myCourse;
+let myCourses;
+let theCourse;
 
 getApi();
+
+setTimeout(function(){
+    buildCourses();
+}, 100)
+
 function getApi() {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         const http = new XMLHttpRequest();
         http.onreadystatechange = () => {
             if (http.readyState == XMLHttpRequest.DONE && http.status == 200) {
-                allCourses = JSON.parse(http.responseText);
-                resolve(allCourses);
+                myCourses = JSON.parse(http.responseText);
+                resolve(myCourses);
             }
         }
         http.open("get", "https://golf-courses-api.herokuapp.com/courses");
@@ -16,8 +21,22 @@ function getApi() {
     });
 }
 
+function buildCourses() {
+    for(let i = 0; i < myCourses.courses.length; i++) {
+        let theCourse = myCourses.courses[i];
+        $(".all-courses").append(`
+            <div class="course" id="${theCourse.id}">
+                <div class="course-name">${theCourse.name}</div>
+                <img src="${theCourse.image}" class="course-image">
+            </div>
+        `);
+    }  
+}
+
+
+
 function getCourse(id) {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         const http = new XMLHttpRequest();
         http.onreadystatechange = () => {
             if (http.readyState == XMLHttpRequest.DONE && http.status == 200) {
@@ -25,7 +44,7 @@ function getCourse(id) {
                 resolve(myCourse);
             }
         }
-        http.open("get", `https://golf-courses-api.herokuapp.com/courses/:${id}`);
+        http.open("get", `https://golf-courses-api.herokuapp.com/courses/${id}`);
         http.send();
     });
 }
